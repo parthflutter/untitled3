fcc3688d7b8e0912ecd64461f77fe4d692a544e2
user <savaliyaparth712@gmail.com>
2023-02-21 20:57:37 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'img.dart';
+
+  void main()
+  {
+    runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: imgfonts(),
+      ),
+    );
+}
